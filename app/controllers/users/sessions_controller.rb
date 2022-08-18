36afe7e412d56bb4dfe44f50160f51682e092da7
yuki saito <ykst0622@gmail.com>
2022-08-18 22:47:29 +0900

    :sparkles: ゲスト管理者ログイン機能を実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
+
   def guest_sign_in
     user = User.guest
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def admin_guest_sign_in
+    user = User.guest_admin
+    sign_in user
+    redirect_to user_path(user.id), notice: 'ゲスト管理ユーザーとしてログインしました'
   end
 end
