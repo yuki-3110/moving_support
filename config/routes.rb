282cf9c157fe5ae3d3d09443d9f8eea1444285b1
yuki saito <ykst0622@gmail.com>
2022-08-19 00:10:24 +0900

    :sparkles: scaffolfでtaskテーブルを生成・viewのレイアウトタスク一覧のリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :tasks
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   root 'homes#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_143637) do
+ActiveRecord::Schema.define(version: 2022_08_18_150411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2022_08_18_143637) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content"
+    t.integer "deadline", default: 0, null: false
+    t.integer "position"
+    t.integer "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
