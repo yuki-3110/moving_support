fcf9b7987dc86caca3e0045c73c011f3205ff371
yuki saito <ykst0622@gmail.com>
2022-08-21 02:12:39 +0900

    :lipstick: ログイン画面のデザイン作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import '../stylesheets/task';
+import '../stylesheets/session';
 import 'bootstrap';
 import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/js/all';
