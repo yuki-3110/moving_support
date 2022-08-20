889a29ec2c011ae040c535bba4011b7d0d2c8520
yuki saito <ykst0622@gmail.com>
2022-08-20 22:29:50 +0900

    :bug: 管理画面のデザイン崩れを解消・管理画面アクセス制限作業中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_admin!, if: :admin_url 
+
 
   def after_sign_in_path_for(resource)
     user_path(resource.id)
   end
+
+  # def admin_url
+  #   request.fullpath.include?("/admin")
+  # end
 
   protected
 
