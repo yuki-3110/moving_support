e53b288b800f430cb885040db44e89db87e61666
yuki saito <ykst0622@gmail.com>
2022-08-20 18:57:16 +0900

    :bug: メモ機能を使用できるよう修正

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -5,37 +5,47 @@ class MemosController < ApplicationController
     # taskをパラメータの値から探し出し,taskに紐づくmemosとしてbuildします。
     # @task = task.find(params[:task_id])
     @memo = @task.memos.build(memo_params)
+    @memo.user_id = current_user.id
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
-      binding.irb
+      # binding.irb
       if @memo.save
         format.js { render :index }
       else
-        format.html { redirect_to task_path(@task), notice: '投稿できませんでした...' }
+        flash.now[:notice] = '100字以内の文字数で入力してください'
+        format.js { render :index }
       end
     end
   end
 
-  def edit
-    @memo = @task.memos.find(params[:id])
-    respond_to do |format|
-      flash.now[:notice] = 'コメントの編集中'
-      format.js { render :edit }
-    end
-  end
+  # respond_to do |format|
+  #   if @task.save
+  #     format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+  #     format.json { render :show, status: :created, location: @task }
+  #   else
+      
 
-  def update
-    @memo = @task.memos.find(params[:id])
-      respond_to do |format|
-        if @memo.update(memo_params)
-          flash.now[:notice] = 'コメントが編集されました'
-          format.js { render :index }
-        else
-          flash.now[:notice] = 'コメントの編集に失敗しました'
-          format.js { render :edit }
-        end
-      end
-  end
+
+  # def edit
+  #   @memo = @task.memos.find(params[:id])
+  #   respond_to do |format|
+  #     flash.now[:notice] = 'コメントの編集中'
+  #     format.js { render :edit }
+  #   end
+  # end
+
+  # def update
+  #   @memo = @task.memos.find(params[:id])
+  #     respond_to do |format|
+  #       if @memo.update(memo_params)
+  #         flash.now[:notice] = 'コメントが編集されました'
+  #         format.js { render :index }
+  #       else
+  #         flash.now[:notice] = 'コメントの編集に失敗しました'
+  #         format.js { render :edit }
+  #       end
+  #     end
+  # end
 
   def destroy
     @memo = Memo.find(params[:id])
@@ -49,7 +59,7 @@ class MemosController < ApplicationController
   private
   # ストロングパラメーター
   def memo_params
-    params.require(:memo).permit(:content, :user_id)
+    params.require(:memo).permit(:content)
   end
 
   def set_task
