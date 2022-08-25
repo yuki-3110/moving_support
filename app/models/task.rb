13583f577265eafeb9aa22f2bac20d9aaf84c5b0
yuki saito <ykst0622@gmail.com>
2022-08-26 00:14:01 +0900

    :rocket: herokuデプロイ後のエラー対処のため、タスクモデルにmoving_id,のバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,6 @@ class Task < ApplicationRecord
   belongs_to :moving
   validates :deadline, presence: true
   validates :title,  presence: true, length: { maximum: 100 }
+  validates :moving_id, presence: true
   enum deadline: {'1か月前まで': 1, '14日前まで': 2, '10日前まで': 3, '1週間前まで': 4, '前日まで': 5, '当日': 6, '1週間後まで': 7, '2週間後まで': 8, '1か月後まで': 9}
 end
