044527de60eba59eebe577ebc176be2a7616c8f6
yuki saito <ykst0622@gmail.com>
2022-08-30 17:04:11 +0900

    :+1: タスク詳細情報欄で、リンクと文章の両方が登録できるように修正

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,5 +1,5 @@
 class Memo < ApplicationRecord
   belongs_to :user
   belongs_to :task
-  validates :content,  presence: true, length: { maximum: 100 }
+  validates :content,  presence: true, length: { maximum: 300 }
 end
